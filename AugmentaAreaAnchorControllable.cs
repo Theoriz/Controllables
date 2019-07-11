4bb6fefa477aab44165f3077b296b13d1e306c1a
alex_bourgeois <abourgeois27380@gmail.com>
2019-07-11 14:26:55 +0200

    Added first controllables

---
+++ AugmentaAreaAnchorControllable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AugmentaAreaAnchorControllable : Controllable {
+
+    [Header("Augmenta Manager Settings")]
+
+    [OSCProperty]
+    [Range(0.0f, 50.0f)]
+    public float distanceToArea;
+
+    [OSCProperty]
+    public float PersonTimeOut;
+	
+    [OSCProperty]
+    public float PositionFollowTightness;
+
+    [OSCProperty]
+    public bool usePhysic;
+
+    [OSCProperty]
+    [Range(1, 20)]
+    public int VelocityAverageValueCount;
+	
+}
---
+++ SharedTextureControllable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharedTextureControllable : Controllable
+{
+    [OSCProperty] public bool SpoutOutput;
+
+    [Header("Output settings")]
+    [Tooltip("in pixels")]
+    [OSCProperty] public int outputWidth;
+    [Tooltip("in pixels")]
+    [OSCProperty] public int outputHeight;
+
+    public override void OnScriptValueChanged(string name)
+    {
+        base.OnScriptValueChanged(name);
+        SpoutOutput = (TargetScript as SharedTexture).enabled;
+    }
+
+    public override void OnUiValueChanged(string name)
+    {
+        base.OnUiValueChanged(name);
+        (TargetScript as SharedTexture).enabled = SpoutOutput;
+    }
+}
+
